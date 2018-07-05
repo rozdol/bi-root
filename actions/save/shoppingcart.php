e257c777c8a173514154c11c1c5f2e8491213f0b
Alex Titoff <rozdol@gmail.com>
2018-07-05 15:37:16 +0300

    sopping cart fixed

--- actions/save/shoppingcart.php
+++ actions/save/shoppingcart.php
@@ -1,10 +1,2 @@
 <?php
 $this->utils->altercart($this->html->readRQ('func'));
-//echo $this->html->pre_display($_POST,"_POST");
-//echo $this->html->pre_display($_GET,"_GET");
-//exit;
-//$this->html->refreshpage('?act=add&table=processdata',1,"<div class='alert alert-info'>Redirecting</div>");
-echo $this->html->pre_display($_SESSION,"_SESSION");
-//exit;
-//echo $this->html->refreshpage("?act=add&table=processdata", 100, "Redirecting");
-//exit;
