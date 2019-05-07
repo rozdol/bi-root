55a127ab74e2073c17bf83c26baab43629d4d093
Alex Titoff <rozdol@gmail.com>
2019-05-07 14:57:52 +0300

    Update mail.php

--- actions/test/mail.php
+++ actions/test/mail.php
@@ -1,5 +1,5 @@
 <?php
-$status=$this->comm->sendgrid('FastConsent:fastconsent@gmail.com', "alex Titov:rozdol@gmail.com", "Testing ".$GLOBALS[settings][rnd], "Test Text ".$GLOBALS[settings][rnd]);
+$status=$this->comm->sendgrid('test:'.$GLOBALS['settings']['system_email'], "Tester:".$GLOBALS['settings']['test_email'], "Testing ".$GLOBALS[settings][rnd], "Test Text ".$GLOBALS[settings][rnd]);
 if ($status==1) {
     echo $this->html->message('Massage sent');
 } else {
