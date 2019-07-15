993f297e37c85fd839a36988a2b9f603723f901d
Alexander Titov <rozdol@gmail.com>
2019-07-15 14:58:36 +0300

    Create end_table_pixie.php

---
+++ helpers/end_table_pixie.php
@@ -0,0 +1,18 @@
+<?php
+$out.=$this->html->tablefoot($i, $totals, $totalrecs);
+if ($dynamic>0) {
+    $nav=$this->html->HT_ajaxpager($totalrecs, $orgqry, "$titleorig.");
+} else {
+    $nav=$this->html->HT_pager($totalrecs, $orgqry);
+}
+if (($i>5)&&($this->html->readRQn('nocart')==0)) {
+    $nav.= $this->html->add_all_to_cart2($what);
+}
+$csv=implode("\n", $csv_arr);
+if ($noexport=='') {
+    if (($csv=='')&&($csv_arr)) {
+        $csv=$this->utils->array_to_csv($csv_arr);
+    }
+    $export=$this->utils->exportcsv($csv);
+}
+$body.="$js $out $nav $export $extra";
