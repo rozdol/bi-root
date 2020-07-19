3c36ccac0b5b309d7f9048ddf3b0f6aacf27e485
Alexander Titov <rozdol@gmail.com>
2020-07-19 21:51:51 +0300

    Update createaccounting.php

--- actions/tools/createaccounting.php
+++ actions/tools/createaccounting.php
@@ -3,12 +3,9 @@
 if (($what == 'createaccounting')&&($access['edit_a_accounts'])){
 	$id=$this->html->readRQn('id');
 	//$pname=$db->GetVal("select newaccounting($id)");
-	
-	$sql="delete from a_accounts where partnerid=$id; 
+	$sql="delete from a_accounts where partnerid=$id;
 	delete from a_transactions where partnerid=$id;";
 	$this->db->GetVal($sql);
-	
-	
 	$curr_id=$this->project->new_accounting($id);
 	$pname=$this->data->get_name('partners',$id);
 	echo"<div class='floatbox'>Accounting for $pname is Initialized.<br><a href='?act=report&what=accounting&id=$id'>Go to accounting</a></div>";
