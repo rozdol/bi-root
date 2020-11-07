855454d02757a6a153bba8e92762beb360867aef
Alexander Titov <rozdol@gmail.com>
2020-11-07 18:01:14 +0200

    delete uploads and docs

--- actions/delete/_default.php
+++ actions/delete/_default.php
@@ -1,24 +1,41 @@
 <?php
 if ($this->data->table_exists($what)) {
     if ($what=="documents") {
-        $objects=$this->db->GetVal("select count(*) from docs2obj where doc_id=$id and ref_id>0 and ref_table!='partners'")*1;
-        if ($objects>0) {
-            $names=$this->data->get_list_csv("select ref_table||':'||ref_id from docs2obj where doc_id=$id and ref_table!='partners'");
-            $name=$this->data->detalize('documents', $id);
-            //$objectslists=$this->utils->F_tostring($this->db->GetResults("select filename from uploads where refid=$id and tablename='documents' and active='1'"),1);
-            $this->html->error("$name can not be deleted, because it has $objects obects attached.<br>$names");
-            exit;
+        $force=$this->html->readRQn('force');
+        if($force>0){
+            $sql="SELECT * FROM uploads where refid=$id and tablename='documents'";
+            if (!($cur = pg_query($sql))) {$this->html->SQL_error($sql);}
+            $rows=pg_num_rows($cur);
+            $id_bkp=$_POST[id];
+            while ($row = pg_fetch_array($cur,NULL,PGSQL_ASSOC)){
+                $i++;
+                echo "$i ($row[id]) $row[name]<br>";
+                $_POST[id]=$row[id];
+                $this->delete('uploads');
+            }
+            $_POST[id]=$id_bkp;
+            //echo "_POST[id]=".$_POST[id]."<br>";
+            $this->db->GetVal("delete from docs2obj where doc_id=$id");
+            $this->db->GetVal("delete from documents where id=$id");
+            //exit;
+        }else{
+            $objects=$this->db->GetVal("select count(*) from docs2obj where doc_id=$id and ref_id>0 and ref_table!='partners'")*1;
+            if ($objects>0) {
+                $names=$this->data->get_list_csv("select ref_table||':'||ref_id from docs2obj where doc_id=$id and ref_table!='partners'");
+                $name=$this->data->detalize('documents', $id);
+                //$objectslists=$this->utils->F_tostring($this->db->GetResults("select filename from uploads where refid=$id and tablename='documents' and active='1'"),1);
+                $this->html->error("$name can not be deleted, because it has $objects obects attached.<br>$names");
+                exit;
+            }
+            $uploads=$this->db->GetVal("select count(*) from uploads where refid=$id and tablename='documents' and active='1'")*1;
+            if ($uploads>0) {
+                $name=$this->data->detalize('documents', $id);
+                $uploadlists=$this->utils->F_tostring($this->db->GetResults("select filename from uploads where refid=$id and tablename='documents' and active='1'"), 1);
+                $this->html->error("$name can not be deleted, because it has $uploads files attached ($uploadlists).<br>");
+                exit;
+            }
+            $this->db->GetVal("delete from docs2obj where doc_id=$id");
         }
-        $uploads=$this->db->GetVal("select count(*) from uploads where refid=$id and tablename='documents' and active='1'")*1;
-        if ($uploads>0) {
-            $name=$this->data->detalize('documents', $id);
-            $uploadlists=$this->utils->F_tostring($this->db->GetResults("select filename from uploads where refid=$id and tablename='documents' and active='1'"), 1);
-            $this->html->error("$name can not be deleted, because it has $uploads files attached ($uploadlists).<br>");
-            exit;
-        }
-        
-
-        $this->db->GetVal("delete from docs2obj where doc_id=$id");
     }
 
     if ($what=="events") {
--- actions/delete/uploads.php
+++ actions/delete/uploads.php
@@ -6,132 +6,166 @@ if($what=="uploads"){
 	$userid=$this->db->GetVar("select userid from uploads where id=$id");
 	if(($uid<>$userid)&&!($access['main_admin'])&&!($access['delete_uploads'])) {$this->html->error("<br><b>No Permission<br>File is not uploaded by you.</b>" );}
 	$uploads=$this->db->GetRow("select * from uploads where id=$id");
-	$filename=$uploads[filename];
 
-	//$thmb=$progdir."/".$this->data->readconfig('thumbdir')."/".$filename;
-	$fullname=$uploaddir."/".$filename;
+	if($uploads[id]==$id){
+		//echo $this->html->pre_display($uploads,"uploads");
+		//exit;
+		$filename=$uploads[filename];
 
-	if($uploads[tablename]=='documents'){
-		$deletedfiles=DATA_DIR.'docs/deleted';
-		$docname=$this->db->GetVal("select name from documents where id=$uploads[refid]");
-		
-		$dirs=explode("-",$docname);
-		$y=$dirs[0];
-		$m=$dirs[1];
-		$d=$dirs[2];
+		//$thmb=$progdir."/".$this->data->readconfig('thumbdir')."/".$filename;
+		$fullname=$uploaddir."/".$filename;
+		if($uploads[tablename]=='documents'){
+			$deletedfiles=DATA_DIR.'docs/deleted';
+			$docname=$this->db->GetVal("select name from documents where id=$uploads[refid]");
 
-		$newdir=$deletedfiles."/$y/$m/$d";
-		if (!is_dir($newdir)) {
-		    if(!mkdir($newdir,0755, true)){
-		        $this->html->error("Failed to make directory $newdir");
-		    }
-		}
-	
-		$docname=str_replace("-","/",$docname);
-		$directory=$deletedfiles."/$docname";
+			$dirs=explode("-",$docname);
+			$y=$dirs[0];
+			$m=$dirs[1];
+			$d=$dirs[2];
 
-		$docname=str_replace("-","/",$docname);
-		$fullname=$uploaddir."/$docname/".$filename;
-
-		$s3_fullname_parts=explode(DS,$fullname);
-		array_shift($s3_fullname_parts);
-		$s3_fullname=implode(DS,$s3_fullname_parts);
-		//echo "Using AWS S3 for $s3_fullname<br>";
-		//
-		//echo $this->html->pre_display($fullname,"fullname"); exit;
-		//$fullname=$deletedfiles."/$docname/".$filename;
-	}
-	
-	$deletedfilename=$filename;
-	$deletedfile=$deletedfiles."/$docname/".$deletedfilename;
-
-	$s3_fullname_parts=explode(DS,$deletedfile);
-	array_shift($s3_fullname_parts);
-	$s3_deleted_fullname=implode(DS,$s3_fullname_parts);
-
-	//echo $this->html->pre_display($s3_deleted_fullname,"s3_deleted_fullname"); exit;
-	$out.= "<br>$fullname<br>$deletedfile";
-	$out.="<br>Moving from $fullname to $deletedfile";
-	if(getenv('AWS_USE_S3')==1){
-		try{
-		    $s3 = new Aws\S3\S3Client([
-		        'region'  => getenv('AWS_REGION'),
-		        'version' => getenv('S3_VERSION'),
-		        'credentials' => [
-		            'key'    => getenv('AWS_KEY'),
-		            'secret' => getenv('AWS_SECRET'),
-		        ]
-		    ]);
-		    //echo "AWS S3 Authenticated<br>";
-		} catch (Aws\S3\Exception\S3Exception $e) {
-		    $message=$e->getMessage();
-		    $parts=explode('<?xml version="1.0" encoding="UTF-8"?>',$message);
-		    $error=$parts[2];
-		    $xml = new SimpleXMLElement($error);
-		    $this->html->error($xml->Message);
-		}
-		try{
-			$s3->copyObject([
-			    'Bucket' 	 => getenv('AWS_S3_BUCKET'),
-			    'Key'        => $s3_deleted_fullname,
-			    'CopySource' => getenv('AWS_S3_BUCKET')."/{$s3_fullname}",
-			]);
-		} catch (Aws\S3\Exception\S3Exception $e) {
-		    $message=$e->getMessage();
-		    //$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
-		    //$this->db->GetVal($sql);
-		    //echo $this->html->pre_display($message,"message"); exit;
-		    $parts=explode('<Error>',$message);
-		    //echo $this->html->pre_display($parts,"parts");
-		    $error=$parts[1];
-		    $this->html->error($error);
-		}
-		try{
-			$s3->deleteObject([
-			    'Bucket' => getenv('AWS_S3_BUCKET'),
-			    'Key'    => $s3_fullname
-			]);
-		} catch (Aws\S3\Exception\S3Exception $e) {
-		    $message=$e->getMessage();
-		    $parts=explode('<?xml version="1.0" encoding="UTF-8"?>',$message);
-		    $error=$parts[2];
-		    $xml = new SimpleXMLElement($error);
-		    $this->html->error($xml->Message);
-		}
-		$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
-
-		//echo "Moved $s3_fullname -> $s3_deleted_fullname<br>";
-
-	}else{
-		if(file_exists($fullname)){
-			if($forever>0){
-				if (unlink($fullname)){
-					$sql="delete from uploads where id=$id";
-				}else{
-					$sql="select 1 as result";
-					$out.="$fullname<br>Not deleted.";
-					echo $out; exit;
-				}
-			}else{
-				if (rename($fullname,$deletedfile)){
-					$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
-				}else{
-					$sql="select 1 as result";
-					$out.="<br>Not deleted.";
-					echo $out; exit;
-				}
+			$newdir=$deletedfiles."/$y/$m/$d";
+			if (!is_dir($newdir)) {
+			    if(!mkdir($newdir,0755, true)){
+			        $this->html->error("Failed to make directory $newdir");
+			    }
 			}
+			$docname=str_replace("-","/",$docname);
+			$directory=$deletedfiles."/$docname";
+
+			$docname=str_replace("-","/",$docname);
+			$fullname=$uploaddir."/$docname/".$filename;
+
+			$s3_fullname_parts=explode(DS,$fullname);
+			array_shift($s3_fullname_parts);
+			$s3_fullname=implode(DS,$s3_fullname_parts);
+			//echo "Using AWS S3 for $s3_fullname<br>";
+			//
+			//echo $this->html->pre_display($fullname,"fullname"); exit;
+			//$fullname=$deletedfiles."/$docname/".$filename;
+		}
+
+		$deletedfilename=$filename;
+		$deletedfile=$deletedfiles."/$docname/".$deletedfilename;
+
+		$s3_fullname_parts=explode(DS,$deletedfile);
+		array_shift($s3_fullname_parts);
+		$s3_deleted_fullname=implode(DS,$s3_fullname_parts);
+
+		//echo $this->html->pre_display($s3_deleted_fullname,"s3_deleted_fullname"); exit;
+
+		if(getenv('AWS_USE_S3')==1){
+			try{
+			    $s3 = new Aws\S3\S3Client([
+			        'region'  => getenv('AWS_REGION'),
+			        'version' => getenv('S3_VERSION'),
+			        'credentials' => [
+			            'key'    => getenv('AWS_KEY'),
+			            'secret' => getenv('AWS_SECRET'),
+			        ]
+			    ]);
+			    //echo "AWS S3 Authenticated<br>";
+			} catch (Aws\S3\Exception\S3Exception $e) {
+			    $message=$e->getMessage();
+			    $parts=explode('<?xml version="1.0" encoding="UTF-8"?>',$message);
+			    $error=$parts[2];
+			    $xml = new SimpleXMLElement($error);
+			    $this->html->error($xml->Message);
+			}
+
+			if($forever>0){
+				try{
+					$s3->deleteObject([
+					    'Bucket' => getenv('AWS_S3_BUCKET'),
+					    'Key'    => $s3_fullname
+					]);
+				} catch (Aws\S3\Exception\S3Exception $e) {
+				    $message=$e->getMessage();
+				    $parts=explode('<?xml version="1.0" encoding="UTF-8"?>',$message);
+				    $error=$parts[2];
+				    $xml = new SimpleXMLElement($error);
+				    $this->html->error($xml->Message);
+				}
+				$sql="delete from uploads where id=$id";
+			}else{
+				try{
+					$s3->copyObject([
+					    'Bucket' 	 => getenv('AWS_S3_BUCKET'),
+					    'Key'        => $s3_deleted_fullname,
+					    'CopySource' => getenv('AWS_S3_BUCKET')."/{$s3_fullname}",
+					]);
+				} catch (Aws\S3\Exception\S3Exception $e) {
+				    $message=$e->getMessage();
+				    //$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
+				    //$this->db->GetVal($sql);
+				    //echo $this->html->pre_display($message,"message"); exit;
+				    $parts=explode('<Error>',$message);
+				    //echo $this->html->pre_display($parts,"parts");
+				    $error=$parts[1];
+				    $this->html->error($error);
+				}
+				try{
+					$s3->deleteObject([
+					    'Bucket' => getenv('AWS_S3_BUCKET'),
+					    'Key'    => $s3_fullname
+					]);
+				} catch (Aws\S3\Exception\S3Exception $e) {
+				    $message=$e->getMessage();
+				    $parts=explode('<?xml version="1.0" encoding="UTF-8"?>',$message);
+				    $error=$parts[2];
+				    $xml = new SimpleXMLElement($error);
+				    $this->html->error($xml->Message);
+				}
+				$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
+			}
+			//echo "Moved $s3_fullname -> $s3_deleted_fullname<br>";
+
 		}else{
 			$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
-			$out.= "<br>File $fullname DOES NOT EXIST";
-		}
-	}
+			if(file_exists($fullname)){
+				if($forever>0){
+					if (unlink($fullname)){
+						$sql="delete from uploads where id=$id";
+					}else{
+						$sql="select 1 as result";
+						$out.="$fullname<br>Not deleted.";
+						echo $out; exit;
+					}
+				}else{
+					$out.= "<br>$fullname<br>$deletedfile";
+					$out.="<br>Moving from $fullname to $deletedfile";
+					if (rename($fullname,$deletedfile)){
+						$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
+					}else{
+						$sql="select 1 as result";
+						$out.="<br>Not deleted.";
+						echo $out; exit;
+					}
+				}
+			}else{
+				if(file_exists($deletedfile)){
+					$out.= "<br>File $fullname is deleted before";
+					if($forever>0){
+						if (unlink($deletedfile)){
+							$sql="delete from uploads where id=$id";
+						}
+					}
 
-	
-	
-	if($uploads[tablename]=='documents'){$tmp=$this->db->GetVal("update documents set uploads=uploads-1 where id=$uploads[refid]");}
-	$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
-	//echo $out; exit;
-	//delete_upload($fullname);
-	//delete_upload($thmb);
+				}else{
+					$out.= "<br>File $fullname DOES NOT EXIST";
+					$sql="delete from uploads where id=$id";
+				}
+				//$sql="update uploads set descr='(deleted)'||descr, active='f' where id=$id";
+
+			}
+		}
+
+		if($uploads[tablename]=='documents'){$tmp=$this->db->GetVal("update documents set uploads=uploads-1 where id=$uploads[refid]");}
+
+		//echo $out; exit;
+		//delete_upload($fullname);
+		//delete_upload($thmb);
+	}else{
+		$out.="<br>No record found with id $id";
+	}
+	echo "$out<br>";
 }
