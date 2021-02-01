44b692a981f676af9e24ba635ed5ee1bc585cc1c
Alexander Titov <rozdol@gmail.com>
2021-02-01 18:52:59 +0200

    Update accounting_renumber.php

--- actions/tools/accounting_renumber.php
+++ actions/tools/accounting_renumber.php
@@ -1,9 +1,11 @@
 <?php
 if ($access['main_admin']){
 	$partnerid=$this->html->readRQ('id')*1;
+	$df=$this->html->readRQd('df',1);
+	$dt=$this->html->readRQd('dt',1);
 	$partnername=$this->data->get_name("partners",$partnerid);
-	$sql="select * from a_transactions where partnerid=$partnerid order by date asc, sorting asc, id asc" ;
-	if (!($cur = pg_query($sql))) {$this->html->SQL_error($sql);}	
+	$sql="select * from a_transactions where partnerid=$partnerid and date>='$df' and date<='$dt' order by date asc, sorting asc, id asc" ;
+	if (!($cur = pg_query($sql))) {$this->html->SQL_error($sql);}
 	while ($row = pg_fetch_array($cur)) {
 		$year=substr($row[date],8,2);
 		if($prevyear!=$year){
@@ -12,9 +14,17 @@ if ($access['main_admin']){
 		}
 		$i++;
 		$name=$year."-".sprintf("%04s",$i);
-		$this->db->GetVal("update a_transactions set name='$name' where id=$row[id]");
-		echo "$row[name] => $name<br>";
+		if($row[name]!=$name){
+			$this->db->GetVal("update a_transactions set name='$name' where id=$row[id]");
+			$det=$this->data->detalize('a_transactions',$row[id]);
+			$sql="update a_transactions set name='$row[name]' where id=$row[id];";
+			$sql_array[]=$sql;
+			echo "$row[name] => $name ($row[date]) $det<br>";
+		}
+
 	}
+	$sqls=implode("\n", $sql_array);
+	echo $this->html->pre_display($sqls,"To revert");
 	echo "Transactions for $partnername are renumbered!";
 }
 ?>
