b0562efdc276f531028e39b249e378738f5c84ad
Alexander Titov <rozdol@gmail.com>
2021-04-11 22:46:45 +0300

    Fix generate menu

--- actions/save/gen_fast_menu.php
+++ actions/save/gen_fast_menu.php
@@ -1,8 +1,5 @@
 <?php
-if ($what == 'gen_fast_menu'){
-	$id=$this->html->readRQn('id');
-	$this->data->gen_fast_menu($id);
-	//$out.= "<pre>";print_r($_POST);$out.= "</pre>";$out.= "<pre>";print_r($vals);$out.= "</pre>";exit;
-}
-
+$id=$this->html->readRQn('id');
+$this->data->gen_fast_menu($id);
+//$out.= "<pre>";print_r($_POST);$out.= "</pre>";$out.= "<pre>";print_r($vals);$out.= "</pre>";exit;
 $body.=$out;
--- actions/save/gen_fast_menu_all.php
+++ actions/save/gen_fast_menu_all.php
@@ -1,16 +1,18 @@
 <?php
-if ($what == 'gen_fast_menu_all'){
-	$sql="select * from groups where id>=2 order by id asc limit 100";
-	if (!($cur = pg_query($sql))) {$this->html->SQL_error($sql);}
-	$rows=pg_num_rows($cur);	
-	while ($row = pg_fetch_array($cur)) {
-		$i++;
-		$this->data->gen_fast_menu($row[id]);
-		$this->livestatus(str_replace("\"","'",$this->html->draw_progress($i/$rows*100)));			
-	}
-	$this->livestatus('');
-	
-	//$out.= "<pre>";print_r($_POST);$out.= "</pre>";$out.= "<pre>";print_r($vals);$out.= "</pre>";exit;
+echo $this->html->message("Generating Menu...");
+$sql="select * from groups where id>=2 order by id asc limit 100";
+if (!($cur = pg_query($sql))) {$this->html->SQL_error($sql);}
+$rows=pg_num_rows($cur);
+while ($row = pg_fetch_array($cur)) {
+	$i++;
+	$this->data->gen_fast_menu($row[id]);
+	//$this->livestatus("Generating Menu for GID:$row[id]");
+	echo $this->html->message("Generating Menu for GID:$row[id]");
+	$this->livestatus(str_replace("\"","'",$this->html->draw_progress($i/$rows*100)));
 }
+$this->livestatus('');
+
+//$out.= "<pre>";print_r($_POST);$out.= "</pre>";$out.= "<pre>";print_r($vals);$out.= "</pre>";exit;
+
 
 $body.=$out;
