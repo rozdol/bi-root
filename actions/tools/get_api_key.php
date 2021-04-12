59990f7414a279d5afe0e80f6a155db76700e8f2
Alexander Titov <rozdol@gmail.com>
2021-04-12 15:33:55 +0300

    Update get_api_key.php

--- actions/tools/get_api_key.php
+++ actions/tools/get_api_key.php
@@ -1,6 +1,6 @@
 <?php
 if ($access['main_admin']){
-	$password=$this->html->readRQp('password');
+	$password=$this->html->readRQ('password');
 	echo $this->html->pre_display($password,"password");
 	$is_client_id=$this->html->readRQn('is_client_id');
 	$is_client=$this->data->get_row('is_clients',$is_client_id);
@@ -8,8 +8,12 @@ if ($access['main_admin']){
 	$api_key=$is_client[api_key];
 	$authorization=$is_client[api_authorization];
 	echo $this->html->pre_display($is_client,"is_client");
+	$api_URL_arr=explode("&",$api_URL);
+	//echo $this->html->pre_display($api_URL_arr,"api_URL_arr");
+	$api_URL=$api_URL_arr[0];
+	//echo $this->html->pre_display($api_URL,"api_URL");exit;
 	$response=$this->comm->api_auth($api_URL, 'api', $password);
-	echo $this->html->pre_display($response,"response");
+	echo $this->html->pre_display($response,"response $api_URL ($password)");
 	if($response->access_token!=''){
 		$vals=[
 			'api_key'=>$response->api_key,
