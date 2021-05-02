12532c2d9a779384b548d61cbba1cef6a622771e
Alexander Titov <rozdol@gmail.com>
2021-05-03 00:21:22 +0300

    Update object_id.php

--- actions/append/object_id.php
+++ actions/append/object_id.php
@@ -1,9 +1,17 @@
 <?php
 if($fieldname=='')$fieldname='id';
-if($table=='')$fieldname='partners';
-$count=$this->db->GetVal("SELECT count(*) FROM $table WHERE lower(name) like lower('%$value%')");
+if($table=='')$table='partners';
+$where_sql="lower(name) like lower('%$value%')";
+if($table=='partners')$where_sql="lower(name) like lower('%$value%') or lower(ru) like lower('%$value%') or lower(en) like lower('%$value%') or lower(synonyms) like lower('%$value%') ";
+$count=$this->db->GetVal("SELECT count(*) FROM $table WHERE $where_sql");
 if($count>0){
-	$sql="SELECT id, name FROM $table WHERE lower(name) like lower('%$value%') ORDER by name";				  
+	$sql="SELECT id, name FROM $table WHERE $where_sql ORDER by name";
+	$sql="SELECT id, CASE WHEN active='t' THEN name
+		ELSE '[inactive] '||name
+		END as name FROM $table WHERE $where_sql ORDER by name";
+	if($table=='partners')$sql="SELECT id, CASE WHEN not ((dateclose>=now() or dateclose is null) and active='t') THEN '[inactive] '||name
+		ELSE name
+		END as name FROM $table WHERE $where_sql ORDER by name";
 	$response=$this->html->htlist($fieldname,$sql,$id,'Select from '.$table,"");
 }else{$response="No $table found";}
 $out.= "$response";
