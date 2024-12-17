a29b80f8e5c9f2c6b2bf2556db31dde8bb6137d9
Alex Titoff <rozdol@gmail.com>
2024-12-17 13:30:29 +0200

    Update createaccounting.php

--- actions/tools/createaccounting.php
+++ actions/tools/createaccounting.php
@@ -6,7 +6,13 @@ if (($what == 'createaccounting')&&($access['edit_a_accounts'])){
 	$sql="delete from a_accounts where partnerid=$id;
 	delete from a_transactions where partnerid=$id;";
 	$this->db->GetVal($sql);
-	$curr_id=$this->project->new_accounting($id);
+	$opt=$this->html->readRQ('opt');
+	if ($opt=='axle'){
+		$curr_id=$this->project->new_accounting_axle($id);
+	}else{
+		$curr_id=$this->project->new_accounting($id);
+	}
+	
 	$pname=$this->data->get_name('partners',$id);
 	echo"<div class='floatbox'>Accounting for $pname is Initialized.<br><a href='?act=report&what=accounting&id=$id'>Go to accounting</a></div>";
 	//echo $this->html->refreshpage("?act=report&what=accounting&id=$id",2,"Accounting for $pname is Initialized.","INFO",'alert alert-info');
