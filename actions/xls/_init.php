8c3502cdded4c0d8aa8a4ca75f07e864ea3d8ff7
Alex Titoff <rozdol@gmail.com>
2025-10-01 21:43:50 +0300

    Update _init.php

--- actions/xls/_init.php
+++ actions/xls/_init.php
@@ -1,9 +1,9 @@
 <?php
-$GLOBALS[pdf_mode]=1;
+$GLOBALS['pdf_mode'] = 1;
 
 setlocale(LC_TIME, "C");
 $tz = @date_default_timezone_get();
-date_default_timezone_set($GLOBALS[settings][timezone]);
+date_default_timezone_set($GLOBALS['settings']['timezone']);
 $fulldate = date("d F Y");
-$date_displayed=date("d.m.Y, H:i:s")." ".$GLOBALS[settings][timezone];
+$date_displayed = date("d.m.Y, H:i:s") . " " . $GLOBALS['settings']['timezone'];
 date_default_timezone_set($tz);
